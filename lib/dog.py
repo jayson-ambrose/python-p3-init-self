9ac064983b3cd077b58733d88976af8ce3b260b2
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-15 20:00:22 -0600

    got it

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Dog:
+    
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    def bark (self):
+        print('Woof!')
+    
+    def get_adopted (self, owner_name):
+        self.owner = owner_name
+
     pass
+
+chewy = Dog("Fido")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
+
 class Person:
+
+    def __init__(self, name):
+        self.name = name
     pass
+
+# class Dog:
+    
+#     def __init__(self, name, breed = "Mutt"):
+#         self.name = name
+#         self.breed = breed
+    
+#     def bark (self):
+#         print('Woof!')
+    
+#     def get_adopted (self, owner_name):
+#         self.owner = owner_name
+
+#     pass
